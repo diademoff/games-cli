8b8c05b2c3c80a3d72d9d0af1b622fcaba745a16
diademoff <diademoff@yandex.ru>
2021-05-14 21:54:58 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snake_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
