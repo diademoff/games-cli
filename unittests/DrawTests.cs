c259391150a25e4587d547edb8d98620b7ebddc3
diademoff <diademoff@yandex.ru>
2021-05-18 18:08:41 +0300

    add line test

--- unittests/DrawTests.cs
+++ unittests/DrawTests.cs
@@ -1,15 +1,25 @@
-using System;
 using Xunit;
+using System.Drawing;
 using Games;
 
 namespace UnitTests
 {
-    public class UnitTest1
+    public class UnitTests
     {
         [Fact]
-        public void Test1()
+        public void CreateLineTest()
         {
-            Assert.True(!false);
+            Line line_horizontal = new Line('*', new Point(0, 0), new Point(0, 3));
+            Line line_vertical = new Line('+', new Point(0, 0), new Point(3, 0));
+
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.True(line_horizontal.ElementContent[i].Char == '*');
+                Assert.True(line_horizontal.ElementContent[i].Location == new Point(0, i));
+
+                Assert.True(line_vertical.ElementContent[i].Char == '+');
+                Assert.True(line_vertical.ElementContent[i].Location == new Point(i, 0));
+            }
         }
     }
 }
