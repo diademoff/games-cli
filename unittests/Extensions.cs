d867127f89ef69064cde7a8e303f01e46821c0d8
diademoff <diademoff@yandex.ru>
2021-07-07 12:57:03 +0300

    add border test

---
+++ unittests/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using Games;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace unittests
+{
+    public static class Extensions
+    {
+        public static bool CheckDrawableLocations(this IDrawable[] input, Point[] locations)
+        {
+            HashSet<Point> array = new HashSet<Point>(input.Select(x => x.Location));
+            HashSet<Point> points = new HashSet<Point>(locations);
+
+            if (array.Count != points.Count)
+                return false;
+
+            foreach (var a in array)
+            {
+                if (!points.Any())
+                    return false;
+                if (!points.Contains(a))
+                    return false;
+                points.Remove(a);
+            }
+
+            return points.Count == 0;
+        }
+    }
+}
